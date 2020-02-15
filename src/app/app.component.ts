48e16a9c6474e5572395d194bcda16e342ad4ad7
JanBricCodera <torridfyre@gmail.com>
2020-02-15 14:20:04 +0800

    practice files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'angular-max';
+  username = "";
+
+  onCheckEmpty() {
+    if (!this.username) return true;
+
+    return false;
+  }
+
+  resetUsername() {
+    this.username = "";
+  }
 }
